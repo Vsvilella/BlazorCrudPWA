1da49041e0e045013a85cbbab48ed93a5b89bebf
matheusFabri <matheus.fabri@viannasempre.com.br>
2023-06-22 11:34:13 -0300

    program.cs atualizado com cors

--- CadastroClienteReservaApi/Program.cs
+++ CadastroClienteReservaApi/Program.cs
@@ -1,6 +1,20 @@
+using CadastroClienteReservaApi.DAOs;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("*");
+                          policy.AllowAnyHeader();
+                          policy.AllowAnyMethod();
+                      });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +29,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+ConfigBd.StringConexao = builder.Configuration.GetConnectionString("Default") ?? "";
 
 app.UseHttpsRedirection();
 
