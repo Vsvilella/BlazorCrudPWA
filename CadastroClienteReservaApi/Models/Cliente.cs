9a195b19211bd05079419e1b44ca9c10b96f226c
Diego Mamede <diegomamede244@gmail.com>
2023-06-23 17:45:20 -0300

    teste

--- CadastroClienteReservaApi/Models/Cliente.cs
+++ CadastroClienteReservaApi/Models/Cliente.cs
@@ -8,9 +8,7 @@ namespace CadastroClienteReservaApi.Models
     public class Cliente : BaseModel
     {
         public string Nome { get; set; } = "";
-
         public string? Documento { get; set; }
-
         public string? Telefone { get; set; }
     }
 }
--- CadastroClienteReservaApi/Models/ClienteReserva.cs
+++ CadastroClienteReservaApi/Models/ClienteReserva.cs
@@ -9,17 +9,13 @@ namespace CadastroClienteReservaApi.Models
         {
             this.cliente = cliente;
         }
-
         public string? IdCliente
         {
             get => cliente == null ? idCliente : cliente.Id;
             set => idCliente = value;
         }
-
         public DateTime DataReserva { get; set; }
-
         public int QuantidadePessoas { get; set; }
-
         private string? idCliente = null;
         private Cliente? cliente;
     }
